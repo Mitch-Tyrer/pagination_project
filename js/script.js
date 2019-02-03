e85b928794b3cc9d19afdf33672335985b432440
Mitch-Tyrer <mltcinematography@gmail.com>
2019-02-02 18:47:32 -0700

    added show page function and comments to explain what I did

--- js/script.js
+++ js/script.js
@@ -16,25 +16,28 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
+const list = document.querySelectorAll('.student-item');
+const itemsPerPage = 10;
+const totalPages = list.length/itemsPerPage;
 
 
 
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
+// show list items function
+const showPage = (arr, page) => {
+   //iterate over the list items
+   for(let i = 0; i < arr.length; i++){
+      //hide them all to start
+      arr[i].style.display = 'none';
+      // declare the first and last items index based on page number
+      // if display is 11 - 20 (index = 10 - 19) ---- first is (2-1)*10 --- last is (2*10) - 1; 
+      let first = (page - 1) * itemsPerPage;
+      let last = (page * itemsPerPage) -1;
+      // show elements if their index is between the first and last element's index
+      if( i >= first && i <= last){
+         arr[i].style.display = 'block';
+      } 
+   }
+}
 
 
 
